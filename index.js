ecb37c9ce3efe63bfd28a666094381b09b4c0ead
Parfaitky <parfaitky13@gmail.com>
2024-08-02 11:43:37 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const sr = ScrollReveal();
+sr.reveal('h5',{
+    duration: 2000,
+    origin: 'left',
+    distance: '30px',
+    scale:'0.5',
+    reset: false
+});
+sr.reveal('.packages .card',{
+    delay:1000,
+    reset:false,
+    origin: 'top',
+    mobile:false
+
+},100);
